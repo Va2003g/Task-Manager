6280050a82f7a135474b55ccb3215bfa0d6362c3
vansh.gupta3 <vansh.gupta@geekyants.com>
2024-05-15 16:28:14 +0530

    Making Login Page Responsive Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,20 +35,20 @@ const Login = () => {
         },
     });
     return (
-        <div className="w-full h-full flex relative justify-evenly">
+        <div className="login w-full h-full flex relative justify-evenly">
             <div className="absolute loginDiv inset-0 opacity-50"></div>
             <div className="image relative top-[168.14px]">
                 <img src={hero} alt="" className="w-[869.32px] h-[495px]" />
             </div>
             <div className="loginBlock flex flex-col relative top-[331px] gap-3">
-                <div className="image">
+                <div className="imageLogo">
                     <img src={logo} alt="" />
                 </div>
                 <div className="message text-[#232324] text-[25.2px] font-[600] leading-[37.8px] tracking-[0.10499993711709976px] text-left">
                     Log In to Your Account
                 </div>
 
-                <div className="btn bg-[#FC5A5A] w-[338px] h-[50px] text-white justify-center flex items-center rounded-xl gap-2 text-sm cursor-pointer" onClick={login}>
+                <div className="btn bg-[#FC5A5A] w-[338px] h-[50px] text-white justify-center flex items-center rounded-xl gap-2 text-sm cursor-pointer hover:bg-red-500" onClick={login}>
                     <img src={Google} alt="" />
                     Continue with Google
                 </div>
