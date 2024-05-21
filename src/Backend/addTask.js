bcf14a97fb6e6a611d44759ccd43322f312b1f10
vansh.gupta3 <vansh.gupta@geekyants.com>
2024-05-21 13:17:11 +0530

    Adding Tasks in Firestore

--- src/Backend/addTask.js
+++ src/Backend/addTask.js
@@ -1,13 +1,16 @@
-import {doc,setDoc} from 'firebase/firestore'
-import { db } from './index'
-
-export const AddTask = async()=>{
+import {query,collection, addDoc,getDocs,where} from 'firebase/firestore'
+import { db,auth} from './index'
+export const AddTask = async(dataArray)=>{
     try{
-        await setDoc(doc(db, "cities", "LA"), {
-            name: "Los Angeles",
-            state: "CA",
-            country: "USA"
-        });
+        const user = auth.currentUser;
+        const queryForFindingUser = query(collection(db, "UserData" ),where("email", "==", user.email));
+        const queryResult = await getDocs(queryForFindingUser);
+        let userId;
+        queryResult.forEach((doc)=>{
+            userId = doc.id;
+        })
+        dataArray.userId = userId;
+        await addDoc(collection(db, `Tasks`), dataArray);
     }catch(err)
     {
         console.log(err);
--- src/components/TaskWindow/TaskWindow.jsx
+++ src/components/TaskWindow/TaskWindow.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Task } from '../../assets'
+import { AddTask } from '../../Backend';
 const TaskWindow = () => {
+
+  const [dataObject,setDataObject]  = useState({
+    Task:"",
+    Category:"",
+    Date:'',
+    Tags:'',
+    Time:''
+  })
+  function addTaskHandler(event)
+  { 
+    event.preventDefault();
+    AddTask(dataObject);
+  }
+  function updateHandler(event)
+  {
+    setDataObject((prevState)=>({
+      ...prevState,
+      [event.target.name] : event.target.value
+    }))
+
+  }
 
   return (
     <div className='bg-[#E4E4E440]/[25%] w-full pt-3 px-10 box-border'>
@@ -12,24 +34,24 @@ const TaskWindow = () => {
           <div className='text-[#EAEAEA] font-[200] w-full'>
             <form className='flex gap-6'> 
               <span className='flex flex-col w-[100%]'>
-                <label For="task" className='relative left-3'>Task</label>
-                <input type="text" id='task' name='Task' placeholder='Task Name..' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6'/>
+                <label For="task" className='relative left-3' >Task</label>
+                <input type="text" id='task' name='Task' placeholder='Task Name..' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6' onChange={updateHandler}/>
                 <label For="tags" className='relative left-2'>Tags</label>
-                <input type="text" id='tags' name='Tags' placeholder='Enter Tags' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6'/>
+                <input type="text" id='tags' name='Tags' placeholder='Enter Tags' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6' onChange={updateHandler}/>
               </span>
               <span className='flex flex-col'>
                 <label For="category" className='relative left-2'>Category</label>
-                <input type="text" id='category' name='Category' placeholder='Enter Category' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6'/>
+                <input type="text" id='category' name='Category' placeholder='Enter Category' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6' onChange={updateHandler}/>
                 <label For="time" className='relative left-2'>Time</label>
-                <input type="text" id='time' name='Time' placeholder='Time for completion' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6'/>
+                <input type="text" id='time' name='Time' placeholder='Time for completion' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6' onChange={updateHandler}/>
               </span>
               <span className='flex flex-col'>
                 <label For="date" className='relative left-2'>Date</label>
-                <input type="date" id='date' name='Date' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6'/>
+                <input type="date" id='date' name='Date' className='bg-slate-100 bg-opacity-[9.28%] text-[#F6F6F6] font-[500] h-[38px] rounded-[6px] focus:outline-none pl-6' onChange={updateHandler}/>
               </span>
               
               <span className='relative top-[1.35rem]'>
-                <button className='bg-[#FFFFFF] text-[#292929] font-[Poppins] font-[600] px-11 py-2 rounded-t-xl rounded-b-xl'>Add</button>
+                <button className='bg-[#FFFFFF] text-[#292929] font-[Poppins] font-[600] px-11 py-2 rounded-t-xl rounded-b-xl' onClick={addTaskHandler}>Add</button>
               </span>
             </form>
           </div>
